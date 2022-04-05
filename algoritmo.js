9f2da8dfd726a9c5ed3b7d2c0e6020b1373f3c80
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-05 11:30:49 -0300

    mi primer commit

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+console.log("hola!");
+
+document.write(" algo ");
+
+document.write("<h1> Mi primer pagina </h1>");
+
+document.write("<img src='https://i.pinimg.com/originals/54/bc/d3/54bcd37dd9353265c6bfa626dda72116.gif' alt='gif - img agregada mediante js'>");
